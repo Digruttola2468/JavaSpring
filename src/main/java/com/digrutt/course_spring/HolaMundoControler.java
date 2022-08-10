7d98895b816b2ead1912866af53c982826601d9a
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-08-09 23:09:05 -0300

    Configurar Spring Boot

--- src/main/java/com/digrutt/course_spring/HolaMundoControler.java
+++ src/main/java/com/digrutt/course_spring/HolaMundoControler.java
@@ -1,2 +1,16 @@
-package com.digrutt.course_spring;public class HolaMundoControler {
+package com.digrutt.course_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class HolaMundoControler {
+
+    //http://localhost:8080/saludar/hola
+    @GetMapping("/hola")
+    public String saludar(){
+        return "Hello World";
+    }
 }
