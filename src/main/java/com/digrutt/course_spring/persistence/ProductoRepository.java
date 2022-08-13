cab40f9fcabb8e547c4a727e8b952427ac15b226
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-08-13 00:54:28 -0300

    Implementar anotacion @Repository

--- src/main/java/com/digrutt/course_spring/persistence/ProductoRepository.java
+++ src/main/java/com/digrutt/course_spring/persistence/ProductoRepository.java
@@ -2,10 +2,19 @@ package com.digrutt.course_spring.persistence;
 
 import com.digrutt.course_spring.persistence.crud.ProductoCrudRepository;
 import com.digrutt.course_spring.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+/*
+@Component Generalizacion en este tipo de anotaciones,
+indicandole que es un componente de Spring pero queda mejor @Repository
+*/
+/*
+@Repository: le indicamos a la clase que es la encarga de interactuar con la base de datos.
+*/
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +28,18 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad,boolean estado){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,estado);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    //Guardar
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+    //Delete
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
